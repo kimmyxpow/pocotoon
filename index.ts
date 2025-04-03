7cf6257382c5cbcc3c9651d4a56f3229b7ee2877
pow <abinovalfauzi@proton.me>
2025-04-03 19:24:01 +0700

    Set page user agent

--- index.ts
+++ index.ts
@@ -34,6 +34,9 @@ async function downloadImage(url: string, filepath: string): Promise<void> {
 
 async function scrapeManhwa(url: string, page: Page): Promise<Manhwa> {
     console.log(`Opening main page: ${url}`);
+    await page.setUserAgent(
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    );
     await page.goto(url, { waitUntil: "domcontentloaded", timeout: 60000 });
 
     // Ambil data dari halaman utama
